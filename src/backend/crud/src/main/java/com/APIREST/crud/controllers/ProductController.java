0c0849a913c8e1cfe62e9f7bcff15ded1dcc35ed
Deglas021 <Douglasmigsil@gmail.com>
2025-06-22 19:50:35 -0300

    termino da API

--- src/backend/crud/src/main/java/com/APIREST/crud/controllers/ProductController.java
+++ src/backend/crud/src/main/java/com/APIREST/crud/controllers/ProductController.java
@@ -3,8 +3,6 @@ package com.APIREST.crud.controllers;
 import com.APIREST.crud.dtos.ProductDto;
 import com.APIREST.crud.model.Product;
 import com.APIREST.crud.repositories.ProductRepositories;
-import org.apache.coyote.Request;
-import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5500")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -32,7 +31,7 @@ public class ProductController {
     public ResponseEntity getById(@PathVariable(value = "id") Integer id) {
         Optional product = repository.findById(id);
         if (product.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.FOUND).body("Product not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
         return ResponseEntity.status(HttpStatus.FOUND).body(product.get());
     }
@@ -44,23 +43,24 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
     }
 
-    @DeleteMapping ("/{id}")
-    public ResponseEntity delete(@PathVariable(value = "id") Integer id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id") Integer id) {
         Optional<Product> product = repository.findById(id);
         if (product.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.FOUND).body("Product not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
         repository.delete(product.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Product deleted");
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
-    @PutMapping ("/{id}")
-    public ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody ProductDto dto){
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody ProductDto dto) {
         Optional<Product> product = repository.findById(id);
         if (product.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.FOUND).body("Product not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
         var productModel = product.get();
-        BeanUtils.copyProperties(dto,productModel);
+        BeanUtils.copyProperties(dto, productModel);
         return ResponseEntity.status(HttpStatus.OK).body(repository.save(productModel));
     }
 
