80ca3b263d4527154e173888bc3fe853496a15c5
Deglas <douglasmigsil@gmail.com>
2025-06-22 11:40:07 -0300

    mexendo na estrutura

--- src/backend/crud/crud/src/main/java/com/APIREST/crud/repositories/ProductRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.APIREST.crud.repositories;
-
-import com.APIREST.crud.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepositories extends JpaRepository<Product,Integer> {
-
-}
--- src/backend/crud/crud/src/main/resources/db/migration/V1__create_table_product.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE product (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    preco NUMERIC(10, 2) NOT NULL,
-    quantidade INTEGER NOT NULL
-);
